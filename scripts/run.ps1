a2e4d62de58ea3e45af610b512ae88216a29bbee
Patrik Sabol <psabol571@gmail.com>
2025-03-24 11:38:26 +0100

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -9,13 +9,30 @@ fi
 ProjectRoot="$(dirname "$(dirname "$0")")"
 export AMBULANCE_API_ENVIRONMENT="Development"
 export AMBULANCE_API_PORT="8080"
+export AMBULANCE_API_MONGODB_USERNAME="root"
+export AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file "${ProjectRoot}/deployments/docker-compose/compose.yaml" $@
+}
 
 case "$command" in
-    "start")
-        go run "${ProjectRoot}/cmd/ambulance-api-service"
-        ;;
     "openapi")
         docker run --rm -ti -v "${ProjectRoot}:/local" openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+        ;;
+    "start")
+        try_mongo_down=false
+        trap 'try_mongo_down=true' EXIT
+        
+        mongo up --detach
+        go run "${ProjectRoot}/cmd/ambulance-api-service"
+        
+        if [ "$try_mongo_down" = true ]; then
+            mongo down
+        fi
+        ;;
+    "mongo")
+        mongo up
         ;;
     *)
         echo "Unknown command: $command" >&2
